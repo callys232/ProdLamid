dd6e1b6cef714d46a66d5d020452ed6c2abedc3d
callys232 <calebjella82@gmail.com>
2025-12-31 03:22:52 +0000

    fixing

---
+++ src/app/postjobs/page.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PostJobs from "@/components/";
+
+const ProfilePage = () => {
+  return (
+    <div>
+      <PostJobs />
+    </div>
+  );
+};
+
+export default ProfilePage;
